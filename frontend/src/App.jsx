53c2f78a8dc1207c051384e98aab295dc72d05a4
japhethh <boknoy32130@gmail.com>
2024-12-10 08:29:52 +0800

    "Fixed typo in import statement for Nav component"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Footer from "./components/Footer";
-import Nav from "./components/nav";
+import Nav from "./components/Nav";
 import Content from "./components/Sections/Content";
 import Content2 from "./components/Sections/Content2";
 import Hero from "./components/Sections/Hero";
